1cb9a6a858229fd0ee74b12f5c00b4bfe9a6ae4f
chrisfender0 <chrisfender0@gmail.com>
2019-02-04 21:56:06 -0800

    Major Changes
    Changed fonts
    Added hidden button with fun features 
    Added Links to Instructables and YouTube 
    Redesigned Home Link
    Redesigned internal carousel links

---
+++ myjavascript.js
@@ -0,0 +1,40 @@
+function huva(){
+    var arrayBuddy =  [evanesco, HalloweenText, goingCrazy];
+    var textArray = ["Watch your pointer !","HalloweenText activated !", "Shake it !"]
+    var fifty_fifty = Math.floor(Math.random() * 3);
+    arrayBuddy[fifty_fifty]();
+    alert(textArray[fifty_fifty]);
+}
+function evanesco(){
+    $( "li" ).hover(function() {
+        $(this).fadeOut(1,null);
+    });
+    $( "img" ).hover(function() {
+        $(this).fadeOut(1,null);
+    });
+    $( "a" ).hover(function() {
+        $(this).fadeOut(1,null);
+    });
+}
+function HalloweenText(){
+    $( "button" ).hover(function(){
+        $(this).addClass("extra");
+    });
+    $( "h3" ).hover(function(){
+        $(this).addClass("extra");
+    });
+    $( "p" ).hover(function(){
+        $(this).addClass("extra");
+    });
+    $( "a" ).hover(function(){
+        $(this).addClass("extra");
+    });
+    $( "nav" ).hover(function(){
+        $(this).addClass("extra");
+    });
+}
+function goingCrazy(){
+    $("img").addClass("moveMe"); 
+    $("a").addClass("moveMe");
+    $("button").addClass("moveMe");
+}
