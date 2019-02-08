7fd80c4df03240b9f222f67e727976c8c4a91c70
chrisfender0 <chrisfender0@gmail.com>
2019-02-07 22:54:48 -0800

    new features
    3 parks added 
    buttons to select parks 
    embedded google maps with specific location and pinning upon park selection
    improved css for parks list

--- ParksList.js
+++ ParksList.js
@@ -1,10 +1,48 @@
-function initMap() {
-  // The location of Wapato
-  var Wapato = {lat: 47.194973, lng: -122.454884};
-  // The map, centered at Wapato
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 15, center: Wapato});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: Wapato, map: map});
+function wapatoClick(){
+    function initMap() {
+    // The location of Wapato
+    var Wapato = {lat: 47.194973, lng: -122.454884};
+    // The map, centered at Wapato
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 15, center: Wapato});
+    // The marker, positioned at Wapato Park
+    var marker = new google.maps.Marker({position: Wapato, map: map});
+    }
+    //set text 
+    document.getElementById("parkInfo").innerHTML = "Wapato Park lies in a beautiful setting of lake and forest. Features include a walking trail around the lake (.9 miles), a playground, a dog park, picnic shelters, and an impressive, historic pergola.";
+    document.getElementById("footnote").innerHTML = "Description provided by MetroParks";
+    //draw map 
+    initMap(); 
 }
-
+function oakTreeParkClick(){
+    function initMap() {
+    // The location of Oak Tree Park
+    var oakTreePark = {lat: 47.190195, lng: -122.476021};
+    // The map, centered at Oak Tre Park
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 15, center: oakTreePark});
+    // The marker, positioned at Oak Tree Park
+    var marker = new google.maps.Marker({position: oakTreePark, map: map});
+    }
+    //set text
+    document.getElementById("parkInfo").innerHTML = "Oak Tree Park is a 25 acre parcel that comprises one of the last remnants of the Garry oak habitat that used to dominate this part of the city.";
+    document.getElementById("footnote").innerHTML = "Description provided by MetroParks";
+    //draw map 
+    initMap();    
+}
+function southParkClick(){
+    function initMap() {
+    // The location of Oak Tree Park
+    var southPark = {lat: 47.212361, lng: -122.482904};
+    // The map, centered at Oak Tre Park
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 15, center: southPark});
+    // The marker, positioned at Oak Tree Park
+    var marker = new google.maps.Marker({position: southPark, map: map});
+    }
+    //set text 
+    document.getElementById("parkInfo").innerHTML = "This forested park serves south Tacoma and features gardens, play equipment, and a sprayground.";
+    document.getElementById("footnote").innerHTML = "Description provided by MetroParks";
+    //draw map 
+    initMap();
+}
---
+++ TacomaParks.js
@@ -0,0 +1,6 @@
+function help(){
+    alert("Use the navigation bar at the top of the site to select a portion of the site. \n\
+Click home to go back to the index page. \n\
+Click park list to get to a selection of parks");
+}
+
